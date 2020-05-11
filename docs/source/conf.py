0ae1aa014b8f9779723c3d3ad786c20dab252d51
pythoro <pythoro@mindquip.com>
2020-05-12 07:53:12 +1200

    [DOC] Add master_doc to conf.py.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,9 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': 4,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -54,3 +57,5 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 autodoc_mock_imports = ['numpy', 'pandas', 'ruamel']
+
+master_doc = 'index'
