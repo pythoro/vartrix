7363d52216b29cd7d137cb0daebc2cae752453ae
pythoro <pythoro@mindquip.com>
2021-03-09 12:22:09 +1300

    [MOD] Update revision.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="vartrix",
-    version="0.0.9",
+    version="0.1.0",
     author="Reuben Rusk",
     description="Easily manange and automate variables and parameters.",
     long_description=long_description,
