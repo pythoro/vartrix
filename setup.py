405a1b30fc94305190fc7ccb5aae2838a011ddbe
pythoro <pythoro@mindquip.com>
2023-01-26 19:56:31 +1300

    Bump version: 0.3.1 → 0.4.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="vartrix",
-    version="0.3.1",
+    version="0.4.0",
     author="Reuben Rusk",
     description="Easily manange and automate variables and parameters.",
     long_description=long_description,
